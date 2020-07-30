481d63e2c09a111a56a3f88a1f09fc9dad6ecb59
CocoBloom <virag.baranyai2@gmail.com>
2020-07-30 09:39:03 +0200

    get_table()

--- server.py
+++ server.py
@@ -1,1 +1,16 @@
-# put your code here
+from flask import Flask, render_template, redirect, request
+
+app = Flask(__name__)
+saved_data = {}
+
+
+@app.route('/')
+def main_page():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(
+        debug=True,  # Allow verbose error reports
+        port=5000  # Set custom port
+    )
